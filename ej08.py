7b097beec52b39c28f1c6f63e3af9cee0d939734
Sergio <zardain.sergio@gmail.com>
2016-08-23 16:57:48 -0300

    Ver con Javier

--- ej08.py
+++ ej08.py
@@ -1,1 +1,8 @@
-#Computar el número K ingresado! 4⋅∑(−1)i+12i . Ayudita: la sumatoria va de 0 a K, en este caso i es el elemento del ciclo.
+# Computar el número K ingresado! 4·?(-1)i+12i . Ayudita: la sumatoria va de 0 a K, en este caso i es el elemento del ciclo.
+# el simbolo es sumatoria de los valores, si se dice sumatoria de 0 a i = 10 de i sería hacer: 0 + 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10
+# la fórmula en si es una cuenta.
+# hace una sumatoria de 0 a i = n de (-1)i + 12i, esto quiere decir que vas a tener que iterar de 0 a n de alguna manera
+# ir sumando los terminos donde reempalazas i por el valor de la iteración. Luego que tengas esa sumatoria, lo multiplicas por 4.
+#Ej:  n = 0, la cuenta da 0. Para n = 1, la cuenta da 4 * (0 + 11) = 44
+4 * ((-1)*1 + 12*(1))
+for x in round(n):
